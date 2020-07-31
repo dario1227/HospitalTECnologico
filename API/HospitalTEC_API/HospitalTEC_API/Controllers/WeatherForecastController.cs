3aa01a579219e79ef27f986f2036ddd129389e1c
Dario Rodriguez <darior1227@gmail.com>
2020-07-31 07:10:29 -0600

    API Funk PostGre

--- API/HospitalTEC_API/HospitalTEC_API/Controllers/WeatherForecastController.cs
+++ API/HospitalTEC_API/HospitalTEC_API/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HospitalTEC_API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,31 +10,19 @@ namespace HospitalTEC_API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : Controller
     {
-        private static readonly string[] Summaries = new[]
+        private readonly MyDbContext context;
+        
+        public WeatherForecastController(MyDbContext context)
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
+            
+            this.context=context;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<Pacientes> GetPacientes()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+            return context.paciente.ToList();
         }
     }
 }
---
+++ API/HospitalTEC_API/HospitalTEC_API/Models/MyDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HospitalTEC_API.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Pacientes> paciente { get; set; }
+    }
+}
